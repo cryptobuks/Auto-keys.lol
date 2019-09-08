7a1ce23c6806a50a1ca1f4e8d2e30908517e98ad
lolapus <37475791+lolapus@users.noreply.github.com>
2019-09-08 09:31:10 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import os
 import re
 
 pages = 0
-print("Loaded Auto keys.lol VERSION 1.1")
+print("Loaded Auto keys.lol VERSION 1.2")
 print("Started search...")
 
 try:
@@ -17,12 +17,11 @@ try:
         for i in result:
             if str(i) + " btc" in str(data):
                 if float(i) > 0:
-                    print(str(i) + " btc")
-                    print(webUrl.geturl())
                     with open('ValidWallets.txt', 'a') as appendFile:
                         appendFile.write('{} btc\n'.format(str(i)))
                         appendFile.write('{}\n'.format(webUrl.geturl()))
         pages = pages + 1
-        print("Pages read: " + str(pages))
+        sys.stdout.write("\rPages read: {}".format(str(pages)))
+        sys.stdout.flush()
 except TimeoutError:
     os.execv(sys.executable, ['python'] + sys.argv)
